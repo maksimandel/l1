2c5a38a7e3d65cfa17b3eb2e3ce4cabaf1f6110a
Maksim <prosviryakov.m@mail.ru>
2022-01-03 18:09:40 +0300

    Lesson-1

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,56 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+<!--    <button v-on:click="counter++">Increase</button>-->
+<!--    <button @click="counter++">Increase</button>-->
+    <button @click="increase(2, $event)">Increase</button>
+    <button @click="resetCounter">Reset Counter</button>
+    {{ counter }}
+    <label for="test">test
+      <input
+          id="test"
+          type="text"
+          @blur="blur"
+          @focus="focus"
+      >
+    </label>
+    <CalculatorComp/>
   </div>
 </template>
 
 <script>
+import CalculatorComp from '@/components/Calc.vue';
 import HelloWorld from './components/HelloWorld.vue';
 
 export default {
   name: 'App',
   components: {
+    CalculatorComp,
     HelloWorld,
+  },
+  data: () => ({
+    counter: 0,
+  }),
+  methods: {
+    // increase: function (step, event) {
+    //   // eslint-disable-next-line no-plusplus
+    //   this.counter += step;
+    //   console.log(event);
+    // },
+    increase(step, event) {
+      // eslint-disable-next-line no-plusplus
+      this.counter += step;
+      console.log(event);
+    },
+    resetCounter() {
+      this.counter = 0;
+    },
+    blur() {
+      console.log('blur');
+    },
+    focus() {
+      console.log('focus');
+    },
   },
 };
 </script>
